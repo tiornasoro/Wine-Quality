97050a3f7b4d2ff029132e7471a7abfba9d1d391
Tiorna SORO <tiornasoro094@gmail.com>
2024-03-19 10:25:47 +0100

    deploiement CICD added

--- src/Wine_Quality/entity/config_entity.py
+++ src/Wine_Quality/entity/config_entity.py
@@ -10,10 +10,14 @@ class DataIngestionConfig:
 
 @dataclass(frozen=True)
 class DataValidationConfig:
-    root_dir:Path
-    source_URL:str 
-    local_data_file: Path 
-    unzip_dir: Path
+    root_dir=Path,
+    STATUS_FILE=str,
+    unzip_data_dir = Path,
+    all_schema=dict,
+     #root_dir:Path
+    #source_URL:str 
+    #local_data_file: Path 
+    #unzip_dir: Path
 
 @dataclass(frozen=True)
 class DataTransformationConfig:
